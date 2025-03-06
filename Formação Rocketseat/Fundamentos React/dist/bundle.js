3d20445e016be3f35a214e15b90c066b96539744
Lenilson Soares <lenilsonp40@gmail.com>
2025-03-05 22:00:56 -0300

    up

---
+++ Formação Rocketseat/Fundamentos React/dist/bundle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _user$adress;
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var user = {
+  name: 'Lenilson'
+};
+console.log((_user$adress = user.adress) === null || _user$adress === void 0 ? void 0 : _user$adress.street);
---
+++ Formação Rocketseat/Fundamentos React/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const user = {
+    name: 'Lenilson',
+}
+
+console.log(user.adress?.street)
