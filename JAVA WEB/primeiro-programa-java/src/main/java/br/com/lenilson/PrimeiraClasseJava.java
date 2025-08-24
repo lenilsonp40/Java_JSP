730f88dd69fc81ce4407624c815010f1c1e3513c
Lenilson Soares <lenilsonp40@gmail.com>
2025-08-24 18:14:46 -0300

    formação Java

---
+++ JAVA WEB/primeiro-programa-java/src/main/java/br/com/lenilson/PrimeiraClasseJava.java
@@ -0,0 +1,18 @@
+package br.com.lenilson;
+
+import javax.swing.*;
+
+public class PrimeiraClasseJava {
+    public static void main(String[] args) {
+
+
+        String nome = JOptionPane.showInputDialog("Informe seu nome: ");
+        System.out.println(nome);
+
+
+        double carrosNumero =  Double.parseDouble(nome);
+        }
+
+
+
+}
